191971df9e607ee6e90720bc08fda89fa19d27dc
예슬_노트북\tulip <tuliphan91@gmail.com>
2022-05-03 22:31:58 +0900

    UI

---
+++ app.py
@@ -0,0 +1,96 @@
+from flask import Flask, render_template, request, jsonify
+app = Flask(__name__)
+
+from pymongo import MongoClient
+client = MongoClient('mongodb+srv://test:sparta@cluster0.3puso.mongodb.net/Cluster0?retryWrites=true&w=majority')
+db = client.dbsparta
+
+
+
+@app.route('/')
+def home():
+   return render_template('index.html')
+
+@app.route('/profile_page')
+def profile():
+   return render_template('profile_page.html')
+
+
+
+####################첫번째 코멘트창########################################
+@app.route("/insta_comment", methods=["POST"])
+def insta_comment_post():
+
+    comment_receive = request.form['comment_give']
+
+    doc = {'comment':comment_receive}
+    db.insta_comment.insert_one(doc)
+    return jsonify({'msg':'소중한 댓글 감사합니다'})
+
+
+@app.route("/insta_comment", methods=["GET"])
+def insta_comment_get():
+    all_comment = list(db.insta_comment.find({}, {'_id': False}))
+    return jsonify({'comments':all_comment})
+
+####################두번째 코멘트창########################################
+@app.route("/insta_comment2", methods=["POST"])
+def insta_comment_post2():
+
+    comment_receive = request.form['comment_give']
+
+    doc = {'comment':comment_receive}
+    db.insta_comment2.insert_one(doc)
+    return jsonify({'msg':'소중한 댓글 감사합니다'})
+
+
+@app.route("/insta_comment2", methods=["GET"])
+def insta_comment_get2():
+    all_comment = list(db.insta_comment2.find({}, {'_id': False}))
+    return jsonify({'comments':all_comment})
+
+####################세번째 코멘트창########################################
+@app.route("/insta_comment3", methods=["POST"])
+def insta_comment_post3():
+
+    comment_receive = request.form['comment_give']
+
+    doc = {'comment':comment_receive}
+    db.insta_comment3.insert_one(doc)
+    return jsonify({'msg':'소중한 댓글 감사합니다'})
+
+
+@app.route("/insta_comment3", methods=["GET"])
+def insta_comment_get3():
+    all_comment = list(db.insta_comment3.find({}, {'_id': False}))
+    return jsonify({'comments':all_comment})
+
+####################네번째 코멘트창########################################
+@app.route("/insta_comment4", methods=["POST"])
+def insta_comment_post4():
+
+    comment_receive = request.form['comment_give']
+
+    doc = {'comment':comment_receive}
+    db.insta_comment4.insert_one(doc)
+    return jsonify({'msg':'소중한 댓글 감사합니다'})
+
+
+@app.route("/insta_comment4", methods=["GET"])
+def insta_comment_get4():
+    all_comment = list(db.insta_comment4.find({}, {'_id': False}))
+    return jsonify({'comments':all_comment})
+
+###################좋아요 갯수 카트########################################
+@app.route("/like_number1", methods=["POST"])
+def post_like_number():
+    like_receive = request.form['like_give1']
+    doc = {'likes1':like_receive}
+    db.like_number1.insert_one(doc)
+    return jsonify({'msg':'좋아요 감사합니다.'})
+
+
+
+if __name__ == '__main__':
+   app.run('0.0.0.0',port=5000,debug=True)
+
---
+++ static/script.js
@@ -0,0 +1,211 @@
+    <!--================1.onclick 하트 좋아요수 증가==================-->
+var counter1=0
+$('#like-heart1').click(function(){
+    counter1++;
+    $('#counter-click1').text(counter1);
+});
+
+var counter2=0
+$('#like-heart2').click(function(){
+    counter2++;
+    $('#counter-click2').text(counter2);
+});
+
+var counter3=0
+    $('#like-heart3').click(function(){
+    counter3++;
+$('#counter-click3').text(counter3);
+});
+
+
+<!--================2. onclick 북마크 색깔 바꾸기==================-->
+
+var bookmark1 = document.getElementById("bookmark1");
+bookmark1.addEventListener("click", function () {
+    this.children[0].style.color = "#88F3FFFF";
+    this.querySelector('.fas').style.fontSize = '3rem';
+    this.getElementsByTagName('I')[0].classList.toggle('fa-spin');
+    this.firstElementChild.style.transition = 'color 1.5s ease 1.25s, font-size 0.75s ease-out';
+}, false);
+var bookmark2 = document.getElementById("bookmark2");
+bookmark2.addEventListener("click", function () {
+    this.children[0].style.color = "#88F3FFFF";
+    this.querySelector('.fas').style.fontSize = '3rem';
+    this.getElementsByTagName('I')[0].classList.toggle('fa-spin');
+    this.firstElementChild.style.transition = 'color 1.5s ease 1.25s, font-size 0.75s ease-out';
+}, false);
+var bookmark3 = document.getElementById("bookmark3");
+bookmark3.addEventListener("click", function () {
+    this.children[0].style.color = "#88F3FFFF";
+    this.querySelector('.fas').style.fontSize = '3rem';
+    this.getElementsByTagName('I')[0].classList.toggle('fa-spin');
+    this.firstElementChild.style.transition = 'color 1.5s ease 1.25s, font-size 0.75s ease-out';
+}, false);
+
+    <!--================3.댓글 보기 토글====================-->
+ $(function() {
+    $('#comment_box1').click(function () {
+        $('#comment-lists1').slideToggle();
+    })
+
+});
+$(function() {
+    $('#comment_box2').click(function () {
+        $('#comment-lists2').slideToggle();
+    })
+
+});
+
+$(function() {
+    $('#comment_box3').click(function () {
+        $('#comment-lists3').slideToggle();
+    })
+
+});
+    <!--================4.댓글 남기기====================-->
+ $(document).ready(function(){
+    show_comment()
+});
+
+function show_comment() {
+    $.ajax({
+        type: "GET",
+        url: "/insta_comment",
+        data: {},
+        success: function (response) {
+            let rows = response['comments']
+
+            for (let i = 0; i < rows.length; i++) {
+                let comment = rows[i]['comment']
+
+                let temp_html = `<p>${comment}</p>`
+                $('#comment-lists1').append(temp_html)
+                $('#comment-number1').text(rows.length)
+
+
+            }
+        }
+    });
+}
+function save_comment() {
+    let comment = $('#uploading_comment1').val()
+    $.ajax({
+        type: 'POST',
+        url: '/insta_comment',
+        data: {comment_give: comment},
+        success: function (response) {
+            alert(response['msg'])
+            window.location.reload()
+        }
+    })
+}
+<!--================4.2 댓글기능 2번째 코멘트====================-->
+ $(document).ready(function(){
+    show_comment2()
+ });
+
+function show_comment2() {
+    $.ajax({
+        type: "GET",
+        url: "/insta_comment2",
+        data: {},
+        success: function (response) {
+            let rows = response['comments']
+
+            for (let i = 0; i < rows.length; i++) {
+                let comment = rows[i]['comment']
+
+                let temp_html = `<p>${comment}</p>`
+                $('#comment-lists2').append(temp_html)
+                $('#comment-number2').text(rows.length)
+
+
+            }
+        }
+    });
+}
+function save_comment2() {
+    let comment = $('#uploading_comment2').val()
+    $.ajax({
+        type: 'POST',
+        url: '/insta_comment2',
+        data: {comment_give: comment},
+        success: function (response) {
+            alert(response['msg'])
+            window.location.reload()
+        }
+    })
+}
+<!--================4.3 3번째 댓글 만들기====================-->
+
+$(document).ready(function(){
+    show_comment3()
+ });
+
+function show_comment3() {
+    $.ajax({
+        type: "GET",
+        url: "/insta_comment3",
+        data: {},
+        success: function (response) {
+            let rows = response['comments']
+
+            for (let i = 0; i < rows.length; i++) {
+                let comment = rows[i]['comment']
+
+                let temp_html = `<p>${comment}</p>`
+                $('#comment-lists3').append(temp_html)
+                $('#comment-number3').text(rows.length)
+
+
+            }
+        }
+    });
+}
+function save_comment3() {
+    let comment = $('#uploading_comment3').val()
+    $.ajax({
+        type: 'POST',
+        url: '/insta_comment3',
+        data: {comment_give: comment},
+        success: function (response) {
+            alert(response['msg'])
+            window.location.reload()
+        }
+    })
+}
+<!--================4.4 4번째 댓글 만들기====================-->
+$(document).ready(function(){
+    show_comment4()
+ });
+
+function show_comment4() {
+    $.ajax({
+        type: "GET",
+        url: "/insta_comment4",
+        data: {},
+        success: function (response) {
+            console.log(response)
+            let rows = response['comments']
+            for (let i = 0; i < rows.length; i++) {
+                let comment = rows[i]['comment']
+                console.log(comment)
+                let temp_html = `<p>${comment}</p>`
+                $('#sli').append(temp_html)
+            }
+
+        }
+    });
+}
+function save_comment4() {
+    let comment = $('#uploading_comment4').val()
+    $.ajax({
+        type: 'POST',
+        url: '/insta_comment4',
+        data: {comment_give: comment},
+        success: function (response) {
+            alert(response['msg'])
+            window.location.reload()
+        }
+    })
+}
